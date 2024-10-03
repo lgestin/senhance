fbb75cc61e5af0461531ac7a49681e1207b2f8e7
lgestin <lucas.gestin.pro@gmail.com>
2024-10-03 21:01:54 +0200

    add resample util

--- src/denoiser/data/utils.py
+++ src/denoiser/data/utils.py
@@ -1,3 +1,4 @@
+import torch
 import torchaudio
 
 
@@ -5,3 +6,9 @@ def load_audio(path: str, start: int = 0, end: int = None):
     num_frames = -1 if end is None else end - start
     audio, sr = torchaudio.load(path, frame_offset=start, num_frames=num_frames)
     return audio, sr
+
+
+def resample(audio: torch.Tensor, orig_sr: int, targ_sr: int):
+    return torchaudio.functional.resample(
+        waveform=audio, orig_freq=orig_sr, new_freq=targ_sr
+    )
