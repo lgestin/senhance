e842266f1300c06b4ace173797cafdc27b8af8f3
lgestin <lucas.gestin.pro@gmail.com>
2024-10-05 11:29:45 +0200

    compute loudness in index and multithreading

--- scripts/data/create_data_index.py
+++ scripts/data/create_data_index.py
@@ -4,30 +4,45 @@ from pathlib import Path
 
 
 from denoiser.data.audio import Audio
-from audiotools import AudioSignal
+from concurrent.futures import ThreadPoolExecutor
 
 
-def create_data_index(data_folder: str, index_path: str = None, split=None):
+def create_data_index(
+    data_folder: str,
+    index_path: str = None,
+    split: str = None,
+    n_workers: int = 1,
+):
     data_folder = Path(data_folder)
-    data = []
 
-    for file in tqdm(data_folder.glob("**/*.wav")):
+    def extract_informations(file: Path):
         relative_path = file.relative_to(data_folder)
-        # audio = AudioSignal(path)
         audio = Audio(file.as_posix())
-        line = {
+
+        info = {
             "filepath": relative_path.as_posix(),
             "sample_rate": audio.sample_rate,
             "duration": audio.duration,
-            # "loudness": audio.loudness(),
+            "loudness": audio.loudness,
         }
-        data.append(line)
+        return info
+
+    with ThreadPoolExecutor(n_workers) as executor:
+        futures = []
+        for file in data_folder.glob("**/*.wav"):
+            future = executor.submit(extract_informations, file)
+            futures.append(future)
+
+    index = []
+    for future in tqdm(futures):
+        info = future.result()
+        index.append(info)
 
     if index_path is None:
         index_path = data_folder / "index.json"
     with open(index_path, "w") as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+        json.dump(index, f, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
-    create_data_index("/home/lucas/data/daps/clean", None)
+    create_data_index("/data/denoising/speech/daps", None, n_workers=8)
