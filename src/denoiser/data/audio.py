9694d15ad870696cd802092ae2f5e2d4749b8e98
lgestin <lucas.gestin.pro@gmail.com>
2024-10-07 15:22:14 +0200

    add mono function

--- src/denoiser/data/audio.py
+++ src/denoiser/data/audio.py
@@ -86,6 +86,12 @@ class Audio:
             ).item()
         return loudness
 
+    def mono(self):
+        waveform = self.waveform
+        waveform = torch.mean(waveform, dim=0, keepdim=True)
+        self._waveform = waveform
+        return self
+
     def resample(self, sample_rate: int):
         if sample_rate != self.sample_rate:
             waveform = torchaudio.functional.resample(
