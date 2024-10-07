d5764ab1c8f7025c329aeaf659b41cd6ecd1e8ac
lgestin <lucas.gestin.pro@gmail.com>
2024-10-07 15:20:54 +0200

    test apply

--- tests/augments/test_background_noise.py
+++ tests/augments/test_background_noise.py
@@ -4,24 +4,32 @@ from denoiser.data.augmentations import BackgroundNoise, BatchAugmentParameters
 
 
 def test_background_noise():
+    generator = torch.Generator().manual_seed(42)
     audio = Audio("/data/denoising/speech/daps/clean/f10_script1_clean.wav")
     augment = BackgroundNoise(
         noise_index_path="/data/denoising/noise/records/musan/noise/index.train.json",
-        min_snr=-5.0,
-        max_snr=25.0,
-        p=1.0,
+        min_snr=-15.0,
+        max_snr=-5.0,
+        p=0.5,
     )
 
-    excerpts = [audio.salient_excerpt(0.5) for _ in range(8)]
+    excerpts = [audio.salient_excerpt(0.5, generator=generator) for _ in range(8)]
     waveforms = torch.stack([excerpt.waveform for excerpt in excerpts])
 
     augment_params = [
-        augment.sample_augment_parameters(excerpt) for excerpt in excerpts
+        augment.sample_augment_parameters(excerpt, generator=generator)
+        for excerpt in excerpts
     ]
     augment_params = BatchAugmentParameters(augment_params)
 
     augmented = augment.augment(waveforms, augment_params)
     assert torch.is_tensor(augmented)
+
+    apply = augment_params.apply
+    for wav, aug in zip(waveforms[apply], augmented[apply]):
+        assert not torch.allclose(wav, aug)
+    for wav, aug in zip(waveforms[~apply], augmented[~apply]):
+        assert torch.allclose(wav, aug)
 
 
 if __name__ == "__main__":
