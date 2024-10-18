4f171e93f26baad8c0757a9238ca3a8e8bab9aa1
lgestin <lucas.gestin.pro@gmail.com>
2024-10-18 19:25:37 +0200

    load audio with soundfile

--- src/denoiser/data/utils.py
+++ src/denoiser/data/utils.py
@@ -1,17 +1,23 @@
 import torch
-import torchaudio
+import torchaudio.functional as F
+import soundfile as sf
 
 
 def load_audio(path: str, start: int = 0, end: int = None):
     num_frames = -1 if end is None else end - start
-    audio, sr = torchaudio.load(path, frame_offset=start, num_frames=num_frames)
+    audio, sr = sf.read(
+        path,
+        start=start,
+        frames=num_frames,
+        dtype="float32",
+        always_2d=True,
+    )
+    audio = torch.from_numpy(audio.T)
     return audio, sr
 
 
 def resample(audio: torch.Tensor, orig_sr: int, targ_sr: int):
-    return torchaudio.functional.resample(
-        waveform=audio, orig_freq=orig_sr, new_freq=targ_sr
-    )
+    return F.resample(waveform=audio, orig_freq=orig_sr, new_freq=targ_sr)
 
 
 def truncated_normal(size: tuple, min_val: float, max_val: float) -> torch.Tensor:
